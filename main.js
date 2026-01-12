da7b1447977759afa0edcde0b56c2966433c7deb
printfhamza <moussa.ramzihamzafr@gmail.com>
2026-01-12 11:16:19 +0100

    Add click event to toggle accordion content

--- main.js
+++ main.js
@@ -1,1 +1,8 @@
+document.querySelectorAll(".card").forEach(card => {
+  card.addEventListener("click", () => {
+    const content = card.querySelector(".accordion-content");
+    content.style.display =
+      content.style.display === "block" ? "none" : "block";
+  });
+});
 
